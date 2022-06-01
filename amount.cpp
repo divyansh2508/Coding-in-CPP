5f0f93df2d6ae802e02d9a51842fdc39ee870957
divyansh2508 <86479436+divyansh2508@users.noreply.github.com>
2022-06-02 00:40:50 +0530

    My first two basic programs in C++

---
+++ amount.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amt1;
+    cout<<"Enter amount1: ";
+    cin>>amt1;
+    int amt2;
+    cout<<"Enter Amount2:"; 
+    cin>>amt2;
+    int sum=amt1+amt2;
+    cout<<"The total sum is: ";
+    cout<<sum;
+}
