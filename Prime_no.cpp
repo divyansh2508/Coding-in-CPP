5d97c52b982770ca5e48f8dc653c83c51abf6a61
divyansh2508 <86479436+divyansh2508@users.noreply.github.com>
2022-07-23 19:59:43 +0530

    To check for a Prime Number

---
+++ Prime_no.cpp
@@ -0,0 +1,24 @@
+//WAP to print whether the entered number is an Prime number or not
+//  Prime number: These are those numbers which either divides by itself or by 1.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, flag;
+    cout<<"Enter any integer: ";
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"The entered integer is not a prime number!!"<<endl;
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"The entered integer is a prime number!!"<<endl;
+    }
+}
