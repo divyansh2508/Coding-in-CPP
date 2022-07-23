055bb7783e49efab2adf19fe0d52dd9b54b21b41
divyansh2508 <86479436+divyansh2508@users.noreply.github.com>
2022-07-23 20:06:28 +0530

    Updated Prime Number

--- Prime_no.cpp
+++ Prime_no.cpp
@@ -1,6 +1,7 @@
 //WAP to print whether the entered number is an Prime number or not
 //  Prime number: These are those numbers which either divides by itself or by 1.
 #include <iostream>
+#include<cmath>
 using namespace std;
 
 int main()
@@ -8,7 +9,7 @@ int main()
     int n, flag;
     cout<<"Enter any integer: ";
     cin>>n;
-    for(int i=2;i<n;i++)
+    for(int i=2;i<=sqrt(n);i++)
     {
         if(n%i==0)
         {
