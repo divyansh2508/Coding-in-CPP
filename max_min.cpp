47336b93af80f857099ea94f184294eca7e866ed
divyansh2508 <86479436+divyansh2508@users.noreply.github.com>
2022-07-26 14:28:33 +0530

    Maximum and Minimum elements of the array.

---
+++ max_min.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter element "<<(i+1)<<": ";
+        cin>>arr[i];
+    }
+    int maxNo=INT_MIN;
+    int minNo=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        maxNo=max(maxNo, arr[i]);
+        minNo=min(minNo, arr[i]);
+    }
+    cout<<"Max number is: "<<maxNo<<endl;
+    cout<<"Min number is: "<<minNo<<endl;
+}
