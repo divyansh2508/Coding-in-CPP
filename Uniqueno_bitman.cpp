b3aca9bd9334af10ce1680e82f696a627f92462b
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2023-06-01 19:00:51 +0530

    Code on Bit Manipulation.
    This code is all about finding a unique number present in the given array. Considering the case that we have only one single unique element in the whole array.

---
+++ Uniqueno_bitman.cpp
@@ -0,0 +1,19 @@
+//WAP to find a unique number in a given array using bit manipulation. Considering the case that we have only one unique value in the whole array.
+
+#include<iostream>
+using namespace std;
+
+int unique(int arr[], int n)
+{
+    int xorsum=0;
+    for(int i=0;i<n;i++)
+        xorsum = xorsum^arr[i];
+    return xorsum;
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,3,2,1};
+    cout<<unique(arr, 7)<<endl;
+    return 0;
+}
